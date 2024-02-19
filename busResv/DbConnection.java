907e0722c0e1556d447beeb9b5201de9b463652b
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-02-19 19:33:13 +0530

    Update DbConnection.java

--- busResv/DbConnection.java
+++ busResv/DbConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DbConnection {
     private static final String url="jdbc:mysql://localhost:8279/busresv";
     private static final String userName = "root";
-    private static final String password = "gtw4t";
+    private static final String password = "*****";
     public static Connection getConnection() throws SQLException{
         return  DriverManager.getConnection(url,userName,password);
     }
